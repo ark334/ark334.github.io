1d97aa0608f23366eb7599bb9f05d20e213aaa3e
Arcadio Ramírez <31579928+ark334@users.noreply.github.com>
2021-11-07 16:03:53 +0000

    Add files via upload

--- tienda/js/app.js
+++ tienda/js/app.js
@@ -19,4 +19,26 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
+function showImages(el) {
+  var windowHeight = jQuery( window ).height();
+  $(el).each(function(){
+      var thisPos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+      if (topOfWindow + windowHeight - 200 > thisPos ) {
+          $(this).addClass("fadeIn");
+      }
+  });
+}
+
+// if the image in the window of browser when the page is loaded, show that image
+$(document).ready(function(){
+  showImages('.star');
+});
+
+// if the image in the window of browser when scrolling the page, show that image
+$(window).scroll(function() {
+  showImages('.star');
+});
+
 document.getElementById("test").children[0].className += " load";
