f55c290d42a2a379eed8bcd12bf760084abfa357
Arcadio Ramírez <31579928+ark334@users.noreply.github.com>
2021-11-07 15:04:51 +0000

    Add files via upload

--- tienda/js/app.js
+++ tienda/js/app.js
@@ -1,7 +1,7 @@
+document.getElementById("test").children[0].className += " load";
+
 const navToggle = document.querySelector('.nav__toggle');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
-
-document.getElementById("test").children[0].className += " load";
