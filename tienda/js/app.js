ee41a05b0f9e7084c30f89ff3c5b7ce3e9369b31
Arcadio Ramírez <31579928+ark334@users.noreply.github.com>
2021-11-12 18:08:50 +0000

    Add files via upload

--- tienda/js/app.js
+++ tienda/js/app.js
@@ -3,6 +3,12 @@ window.addEventListener("scroll", closeMenu);
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
@@ -41,4 +47,110 @@ $(window).scroll(function() {
   showImages('.star');
 });
 
-document.getElementById("test").children[0].className += " load";
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
+
+var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+var collapseList = collapseElementList.map(function (collapseEl) {
+  return new bootstrap.Collapse(collapseEl)
+})
+
+function fun1() {
+  document.getElementById("select").style.display = "none";
+  document.getElementById("2010").style.backgroundColor = "darkgreen";
+  document.getElementById("a2012").classList.remove("show");
+  document.getElementById("a2014").classList.remove("show");
+  document.getElementById("2012").style.backgroundColor = "lightskyblue";
+  document.getElementById("2014").style.backgroundColor = "lightskyblue";
+  location.href = "#";
+  location.href = "#2010";
+}
+
+function fun2() {
+  document.getElementById("select").style.display = "none";
+  document.getElementById("2012").style.backgroundColor = "lightgreen";
+  document.getElementById("a2010").classList.remove("show");
+  document.getElementById("a2014").classList.remove("show");
+  document.getElementById("2010").style.backgroundColor = "lightskyblue";
+  document.getElementById("2014").style.backgroundColor = "lightskyblue";
+  location.href = "#";
+  location.href = "#2012";
+}
+
+function fun3() {
+  document.getElementById("select").style.display = "none";
+  document.getElementById("2014").style.backgroundColor = "lightgreen";
+  document.getElementById("a2010").classList.remove("show");
+  document.getElementById("a2012").classList.remove("show");
+  document.getElementById("2010").style.backgroundColor = "lightskyblue";
+  document.getElementById("2012").style.backgroundColor = "lightskyblue";
+  location.href = "#";
+  location.href = "#2014";
+}
+
+function fun4() {
+  document.getElementById("select").style.display = "none";
+  document.getElementById("2014").style.backgroundColor = "lightgreen";
+  document.getElementById("a2010").classList.remove("show");
+  document.getElementById("a2012").classList.remove("show");
+  document.getElementById("2010").style.backgroundColor = "lightskyblue";
+  document.getElementById("2012").style.backgroundColor = "lightskyblue";
+  location.href = "#";
+  location.href = "#2014";
+}
+
+function fun5() {
+  document.getElementById("select").style.display = "none";
+  document.getElementById("2014").style.backgroundColor = "lightgreen";
+  document.getElementById("a2010").classList.remove("show");
+  document.getElementById("a2012").classList.remove("show");
+  document.getElementById("2010").style.backgroundColor = "lightskyblue";
+  document.getElementById("2012").style.backgroundColor = "lightskyblue";
+  location.href = "#";
+  location.href = "#2014";
+}
