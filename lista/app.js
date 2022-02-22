90035f85cfa7d72f9db86d0c79a1a157866afc1b
Arcadio Ramírez <31579928+ark334@users.noreply.github.com>
2022-02-22 12:52:16 +0000

    Add files via upload

--- lista/app.js
+++ lista/app.js
@@ -26,7 +26,7 @@ const add1 = () => {
             document.getElementById("info").innerText = "";
             document.getElementById("info").style.backgroundColor= "";
         }, 2000);
-        return;
+        // return;
     }
 
     document.getElementById("oculta").style.opacity = "1";
@@ -55,10 +55,9 @@ const add2 = () => {
             document.getElementById("info").innerText = "";
             document.getElementById("info").style.backgroundColor= "";
         }, 2000);
-        return;
+        // return;
     }
     else if (!lista.includes(art.trim().toUpperCase())) {
-        // if (art[art.length-1] === " " || art[0] === " ") console.log("espacio");
         lista.push(art.trim().toUpperCase());
         prices.push(Number(price));
         document.getElementById("precio").value = "";
@@ -73,7 +72,7 @@ const add2 = () => {
             document.getElementById("info").innerText = "";
             document.getElementById("info").style.backgroundColor= "";
         }, 2000);
-        return;
+        // return;
     }
 
     document.getElementById("oculta").style.opacity = "1";
